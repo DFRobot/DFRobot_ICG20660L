ed997dce24f170c3ef9ebcb1fefe9666f312a0ad
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-08-20 10:02:16 +0800

    Update demo_get_sensor_data.py

--- python/raspberrypi/examples/demo_get_sensor_data.py
+++ python/raspberrypi/examples/demo_get_sensor_data.py
@@ -2,7 +2,7 @@
 
 '''
   # @file demo_get_sensor_data.py
-  # @brief 获取传感器的陀螺仪、加速度和板载温度。
+  # @brief Get the sensor's gyroscope, accelerometer, and onboard temperature.
   # @n Hardware conneted table in SPI
   # @n --------------------------------------------------------------
   # @n  Sensor      |             MCU                | raspberry pi |
@@ -58,15 +58,15 @@ icg = DFRobot_ICG20660L_SPI(cs = 22)
 
 if __name__ == "__main__":
   '''
-    @brief 初始化传感器，初始化后，所有传感器都被关闭，需通过enable_sensor打开相应的配置.
-    @param mode: 配置读取传感器数据是从FIFO还是从寄存器。
+    @brief Initialize the sensor. After initialization, all sensors are turned off, and the corresponding configuration needs to be turned on through enableSensor.. 
+    @param mode: Does configuration read sensor data from FIFO or register?
     @n     eREG_MODE :   Read sensor data from data register.
-    @n     eFIFO_MODE:   Read sensor data from 512 bytes FIFO. Note:从FIFO读取，加速度，陀螺仪、温度必须全部使能，
-    @n 且将其内部采样率必须配置成一致
+    @n     eFIFO_MODE:   Read sensor data from 512 bytes FIFO. Note:Read from FIFO, accelerometer, gyroscope, and temperature must all be enabled,
+    @n and the internal sampling rate must be configured to be consistent. 
     @return status:
     @n      0 : Initialization sucess.
     @n      -1: Interface Initialization failed(IIC or SPI).
-    @n      -2: 读取设备ID失败，ID不是0x91
+    @n      -2: Failed to read the device ID, the ID is not 0x91
   '''
   while icg.begin(icg.eREG_MODE) != 0:
     print("Initialization 6-axis sensor failed.")
@@ -75,7 +75,7 @@ if __name__ == "__main__":
   print("ICG20660L Device ID: %#x"%icg.read_id())
   '''
     @brief Enable sensor, Include Accel of xyz axis, Gyro of xyz, temperature and fifo low power enable bit. 
-    @param bit: 8位字节数据，每一位都代表使能一个功能位，如下表所示：
+    @param bit: 8-bit byte data. Each bit represents enabling a function bit, as shown in the following table:
     @n -------------------------------------------------------------------------------------------------------------------
     @n |        bit7      |     bit6     |      bit5   |    bit4     |     bit3    |     bit2   |    bit1    |    bit0    |
     @n -------------------------------------------------------------------------------------------------------------------
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     @n   bit5:  Z-axis of acceleration.
     @n   bit6:  reserve.
     @n   bit7:  reserve.
-    @n Note: 使能陀螺仪的任意轴，都会自动使能传感器板载温度传感器。
+    @n Note: Enabling any axis of the gyroscope will automatically enable the on-board temperature sensor.
     @n   eGYRO_AXIS_Z: The bit0 of the bit, enable gyro's z axis and temperature.
     @n   eGYRO_AXIS_Y: The bit1 of the bit, enable gyro's y axis and temperature.
     @n   eGYRO_AXIS_X: The bit2 of the bit, enable gyro's X axis and temperature.
@@ -112,13 +112,14 @@ if __name__ == "__main__":
     @n     eFSR_G_250DPS:  The full scale range is ±250 dps.
     @n     eFSR_G_500DPS:  The full scale range is ±500 dps.
     @param bd  Set 3-db bandwidth.
-    @n     eGYRO_DLPF_8173_32KHZ:    当信号等于或大于8173Hz时，会出现明显衰减，衰减3-db，内部采样率为32KHz
-    @n     eGYRO_DLPF_3281_32KHZ: 当信号等于或大于3281Hz时，会出现明显衰减，衰减3-db，内部采样率为32KHz
-    @n     eGYRO_DLPF_250_8KHZ:     当信号等于或大于250Hz时，会出现明显衰减，衰减3-db，内部采样率为8KHz
-    @n     eGYRO_DLPF_176_1KHZ:     当信号等于或大于176Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eGYRO_DLPF_92_1KHZ:      当信号等于或大于92Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eGYRO_DLPF_3281_8KHZ:  当信号等于或大于3281Hz时，会出现明显衰减，衰减3-db，内部采样率为8KHz
-    @n 注意：当陀螺仪和加速度都使能的时候，如果通过FIFO读取传感器数据，必须保证陀螺仪和加速度的内部采样率一致
+    @n     eGYRO_DLPF_8173_32KHZ:   When the signal is equal to or greater than 8173Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 32KHz.
+    @n     eGYRO_DLPF_3281_32KHZ:   When the signal is equal to or greater than 3281Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 32KHz.
+    @n     eGYRO_DLPF_250_8KHZ:     When the signal is equal to or greater than 250Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 8KHz.
+    @n     eGYRO_DLPF_176_1KHZ:     When the signal is equal to or greater than 176Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eGYRO_DLPF_92_1KHZ:      When the signal is equal to or greater than 92Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eGYRO_DLPF_3281_8KHZ:  When the signal is equal to or greater than 3281Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 8KHz.
+    @n Note: When the gyroscope and accelerometer are both enabled, if the sensor data is read through the FIFO, the internal sampling rate of the gyroscope and accelerometer must be the same.
+ */
   '''
   icg.config_gyro(scale = icg.eFSR_G_500DPS, bd = icg.eGYRO_DLPF_176_1KHZ)
   '''
@@ -129,17 +130,17 @@ if __name__ == "__main__":
     @n     eFSR_A_8G:  The full scale range is ±8g.
     @n     eFSR_A_16G:  The full scale range is ±16g.
     @param bd  Set 3-db bandwidth.
-    @n     eACCEL_DLPF_5_1KHZ or 0:    当信号小于或等于5Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eACCEL_DLPF_10_1KHZ or 1:   当信号小于或等于10Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eACCEL_DLPF_21_1KHZ or 2:   当信号小于或等于21Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eACCEL_DLPF_44_1KHZ or 3:   当信号小于或等于44Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eACCEL_DLPF_99_1KHZ or 4:   当信号小于或等于99Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eACCEL_DLPF_218_1KHZ or 5:  当信号小于或等于218Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz，支持低功耗模式
-    @n     eACCEL_DLPF_420_1KHZ or 6:  当信号小于或等于420Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz，支持低功耗模式
-    @n     eACCEL_DLPF_1046_4KHZ or 7: 当信号小于或等于1046Hz时，会出现明显衰减，衰减3-db，内部采样率为4KHz，支持低功耗模式
-    @n     eACCEL_DLPF_55_1KHZ or 8:   当信号小于或等于55Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz，仅支持低功耗模式
-    @n     eACCEL_DLPF_110_1KHZ or 9:  当信号小于或等于110Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz，仅支持低功耗模式
-    @n 注意：当陀螺仪和加速度都使能的时候，如果通过FIFO读取传感器数据，必须保证陀螺仪和加速度的内部采样率一致
+    @n     eACCEL_DLPF_5_1KHZ or 0:    When the signal is less than or equal to 5Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eACCEL_DLPF_10_1KHZ or 1:   When the signal is less than or equal to 10Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eACCEL_DLPF_21_1KHZ or 2:   When the signal is less than or equal to 21Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eACCEL_DLPF_44_1KHZ or 3:   When the signal is less than or equal to 44Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eACCEL_DLPF_99_1KHZ or 4:   When the signal is less than or equal to 99Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eACCEL_DLPF_218_1KHZ or 5:  When the signal is less than or equal to 218Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz. Support low power consumption
+    @n     eACCEL_DLPF_420_1KHZ or 6:  When the signal is less than or equal to 420Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz. Support low power consumption
+    @n     eACCEL_DLPF_1046_4KHZ or 7: When the signal is less than or equal to 1046Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 4KHz. Support low power consumption
+    @n     eACCEL_DLPF_55_1KHZ or 8:   When the signal is less than or equal to 55Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz. Only support low power consumption
+    @n     eACCEL_DLPF_110_1KHZ or 9:  When the signal is less than or equal to 110Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz. Only support low power consumption
+    @n Note: When the gyroscope and accelerometer are both enabled, if the sensor data is read through the FIFO, the internal sampling rate of the gyroscope and accelerometer must be the same.
     @param odr:  Sets the frequency of waking up the chip to take a sample of accel data – the low power accel Output Data Rate.
     @n     eODR_125Hz or 9:    The low power accel Output Data Rate: 125Hz
     @n     eODR_250Hz or 10:   The low power accel Output Data Rate: 250Hz
@@ -152,8 +153,8 @@ if __name__ == "__main__":
   '''
     @brief Set sample rate divider. 
     @param div  Sample rate divider, the range is 0~255.
-    @n     采样率 = 内部采样率/(div+1)
-    @n Note: 如果加速度配置为低功耗模式，即configAccel函数的形参lowPowerFlag为true，则采样率必须和configAccel的形参odr输出率相匹配，如下表所示：
+    @n      Sampling rate = internal sampling rate/(div+1)
+    @n Note: If the accelerometer configuration is in low power consumption mode, that is, the formal parameter lowPowerFlag of the configAccel function is true, the sampling rate must match the output rate of the formal parameter odr of configAccel , as shown in the following table:
     @n ----------------------------------------------------------------------------
     @n |                        config_accel                      | set_sample_div |
     @n ----------------------------------------------------------------------------|
@@ -163,7 +164,7 @@ if __name__ == "__main__":
     @n ----------------------------------------------------------------------------|
     @n |                           |  eODR_125Hz   |    true      |        7       |
     @n |                           |-----------------------------------------------|
-    @n |  支持低功耗模式的bd       |  eODR_250Hz   |    true      |        3       |
+    @n |bd of supporting low power consumption mode|  eODR_250Hz   |    true      |        3       |
     @n |                           |-----------------------------------------------|
     @n |                           |  eODR_500Hz   |    true      |        1       |
     @n |---------------------------------------------------------------------------|
