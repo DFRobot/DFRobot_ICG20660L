6b30242e114c16fb82ce9502b17bc45248ab520c
Frances9 <44355133+Frances9@users.noreply.github.com>
2021-08-20 09:50:32 +0800

    Update demo_get_gyro_data.py

--- python/raspberrypi/examples/demo_get_gyro_data.py
+++ python/raspberrypi/examples/demo_get_gyro_data.py
@@ -2,7 +2,7 @@
 
 '''
   # @file demo_get_gyro_data.py
-  # @brief 获取传感器的陀螺仪数据(此demo不支持FIFO读取模式)。
+  # @brief Obtain data of the sensor's gyroscope (this demo does not support FIFO reading mode)
   # @n Hardware conneted table in SPI
   # @n --------------------------------------------------------------
   # @n  Sensor      |             MCU                | raspberry pi |
@@ -58,15 +58,15 @@ icg = DFRobot_ICG20660L_SPI(cs = 22)
 
 if __name__ == "__main__":
   '''
-    @brief 初始化传感器，初始化后，所有传感器都被关闭，需通过enable_sensor打开相应的配置.
-    @param mode: 配置读取传感器数据是从FIFO还是从寄存器。
+    @brief Initialize the sensor. After initialization, all sensors are turned off, and the corresponding configuration needs to be turned on through enableSensor.
+    @param mode: Does configuration read sensor data from FIFO or register?
     @n     eREG_MODE :   Read sensor data from data register.
-    @n     eFIFO_MODE:   Read sensor data from 512 bytes FIFO. Note:从FIFO读取，加速度，陀螺仪、温度必须全部使能，
-    @n 且将其内部采样率必须配置成一致
+    @n     eFIFO_MODE:   Read sensor data from 512 bytes FIFO. Note: Read from FIFO, accelerometer, gyroscope, and temperature must all be enabled
+    @n and the internal sampling rate must be configured to be consistent.
     @return status:
     @n      0 : Initialization sucess.
     @n      -1: Interface Initialization failed(IIC or SPI).
-    @n      -2: 读取设备ID失败，ID不是0x91
+    @n      -2: Failed to read the device ID, the ID is not 0x91
   '''
   while icg.begin(icg.eREG_MODE) != 0:
     print("Initialization 6-axis sensor failed.")
@@ -75,7 +75,7 @@ if __name__ == "__main__":
   print("ICG20660L Device ID: %#x"%icg.read_id())
   '''
     @brief Enable sensor, Include Accel of xyz axis, Gyro of xyz, temperature and fifo low power enable bit. 
-    @param bit: 8位字节数据，每一位都代表使能一个功能位，如下表所示：
+    @param bit: 8-bit byte data. Each bit represents enabling a function bit, as shown in the following table:
     @n -------------------------------------------------------------------------------------------------------------------
     @n |        bit7      |     bit6     |      bit5   |    bit4     |     bit3    |     bit2   |    bit1    |    bit0    |
     @n -------------------------------------------------------------------------------------------------------------------
@@ -91,7 +91,7 @@ if __name__ == "__main__":
     @n   bit5:  Z-axis of acceleration.
     @n   bit6:  reserve.
     @n   bit7:  reserve.
-    @n Note: 使能陀螺仪的任意轴，都会自动使能传感器板载温度传感器。
+    @n Note: Enabling any axis of the gyroscope will automatically enable the on-board temperature sensor.
     @n   eGYRO_AXIS_Z: The bit0 of the bit, enable gyro's z axis and temperature.
     @n   eGYRO_AXIS_Y: The bit1 of the bit, enable gyro's y axis and temperature.
     @n   eGYRO_AXIS_X: The bit2 of the bit, enable gyro's X axis and temperature.
@@ -111,20 +111,20 @@ if __name__ == "__main__":
     @n     eFSR_G_250DPS:  The full scale range is ±250 dps.
     @n     eFSR_G_500DPS:  The full scale range is ±500 dps.
     @param bd  Set 3-db bandwidth.
-    @n     eGYRO_DLPF_8173_32KHZ:    当信号等于或大于8173Hz时，会出现明显衰减，衰减3-db，内部采样率为32KHz
-    @n     eGYRO_DLPF_3281_32KHZ: 当信号等于或大于3281Hz时，会出现明显衰减，衰减3-db，内部采样率为32KHz
-    @n     eGYRO_DLPF_250_8KHZ:     当信号等于或大于250Hz时，会出现明显衰减，衰减3-db，内部采样率为8KHz
-    @n     eGYRO_DLPF_176_1KHZ:     当信号等于或大于176Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eGYRO_DLPF_92_1KHZ:      当信号等于或大于92Hz时，会出现明显衰减，衰减3-db，内部采样率为1KHz
-    @n     eGYRO_DLPF_3281_8KHZ:  当信号等于或大于3281Hz时，会出现明显衰减，衰减3-db，内部采样率为8KHz
-    @n 注意：当陀螺仪和加速度都使能的时候，如果通过FIFO读取传感器数据，必须保证陀螺仪和加速度的内部采样率一致
+    @n     eGYRO_DLPF_8173_32KHZ:    When the signal is equal to or greater than 8173Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 32KHz.
+    @n     eGYRO_DLPF_3281_32KHZ:    When the signal is equal to or greater than 3281Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 32KHz.
+    @n     eGYRO_DLPF_250_8KHZ:      When the signal is equal to or greater than 250Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 8KHz.
+    @n     eGYRO_DLPF_176_1KHZ:      When the signal is equal to or greater than 176Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eGYRO_DLPF_92_1KHZ:       When the signal is equal to or greater than 92Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 1KHz.
+    @n     eGYRO_DLPF_3281_8KHZ:     When the signal is equal to or greater than 3281Hz, there will be obvious attenuation, 3-db attenuation, and the internal sampling rate is 8KHz.
+    @n Note: When the gyroscope and accelerometer are both enabled, if the sensor data is read through the FIFO, the internal sampling rate of the gyroscope and accelerometer must be the same.
   '''
   icg.config_gyro(scale = icg.eFSR_G_500DPS, bd = icg.eGYRO_DLPF_8173_32KHZ)
   '''
     @brief Set sample rate divider. 
     @param div  Sample rate divider, the range is 0~255.
-    @n     采样率 = 内部采样率/(div+1)
-    @n Note: 如果加速度配置为低功耗模式，即configAccel函数的形参lowPowerFlag为true，则采样率必须和configAccel的形参odr输出率相匹配，如下表所示：
+    @n     Sampling rate = internal sampling rate/(div+1)
+    @n Note: If the accelerometer configuration is in low power consumption mode, that is, the formal parameter lowPowerFlag of the configAccel function is true, the sampling rate must match the output rate of the formal parameter odr of configAccel , as shown in the following table:
     @n ----------------------------------------------------------------------------
     @n |                        config_accel                      | set_sample_div |
     @n ----------------------------------------------------------------------------|
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     @n ----------------------------------------------------------------------------|
     @n |                           |  eODR_125Hz   |    true      |        7       |
     @n |                           |-----------------------------------------------|
-    @n |  支持低功耗模式的bd       |  eODR_250Hz   |    true      |        3       |
+    @n | bd of supporting low power consumption mode|  eODR_250Hz   |    true      |        3       |
     @n |                           |-----------------------------------------------|
     @n |                           |  eODR_500Hz   |    true      |        1       |
     @n |---------------------------------------------------------------------------|
